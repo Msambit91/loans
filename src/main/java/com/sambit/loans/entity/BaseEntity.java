2bf404b3663d62b1e841e7f32d3b2f628d94ebfc
msambit91 <sambitstudy91@gmail.com>
2025-08-07 01:08:24 +0530

    Entity classes added

---
+++ src/main/java/com/sambit/loans/entity/BaseEntity.java
@@ -0,0 +1,34 @@
+package com.sambit.loans.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter @Setter @ToString
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime updatedAt;
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    @Column(insertable = false)
+    private String updatedBy;
+}
